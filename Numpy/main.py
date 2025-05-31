9f7d0a93b7ee5ae1e56f9b37afa232360cd3efef
Chinmay Bhatt <123961463+ChinmayBhattt@users.noreply.github.com>
2025-05-31 18:34:34 +0530

    Add files via upload

---
+++ Numpy/main.py
@@ -0,0 +1,180 @@
+<div align="center">
+  <table>
+    <tr>
+      <td>
+        <a href="https://github.com/chinmaybhattt/github-readme-stats"><img alt="Chinmay Github Stats" src="https://github-readme-stats.vercel.app/api?username=chinmaybhattt&show_icons=true&count_private=true&theme=react&hide_border=true&bg_color=0D1117" width="580" /></a>
+  <a href="https://github.com/chinmaybhattt/github-readme-stats" ><img alt="Chinmay github stats" src="https://github-readme-stats.vercel.app/api/top-langs/?username=chinmaybhattt&langs_count=8&count_private=true&layout=compact&theme=react&hide_border=true&bg_color=0D1117"  width="389" /></a>
+      </td>
+    </tr>
+  </table>
+  
+</div>
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------
+## 📊 My Github Stats
+
+   <br/>
+<a href="https://github.com/chinmaybhattt/github-readme-stats"><img alt="Prince Singh Github Stats" src="https://github-readme-stats.vercel.app/api?username=chinmaybhattt&show_icons=true&count_private=true&theme=react&hide_border=true&bg_color=0D1117" width="600" /></a>
+  <a href="https://github.com/chinmaybhattt/github-readme-stats" ><img alt="Prince Singh Top Languages" src="https://github-readme-stats.vercel.app/api/top-langs/?username=chinmaybhattt&langs_count=8&count_private=true&layout=compact&theme=react&hide_border=true&bg_color=0D1117" width="407" /></a>
+  <br/>
+
+
+# --------------------------------------------------------------------------------------------------
+
+<!-- chinmay bhatt -->
+  
+<div align="center">
+
+<!-- <img src="https://github.com/ChinmayBhattt/chinmaybhattt/blob/main/Images/ChinmayAnimatedBanner.gif" alt="Chinmay Animated Banner" height=160 /> -->
+  <!-- Uncomment the section below to include the snake animation -->
+  <!-- 
+  <h2>🐍 Contributions 🐍</h2>
+  <img alt="snake eating my contributions" src="https://raw.githubusercontent.com/salesp07/salesp07/output/github-contribution-grid-snake.svg" />
+  -->
+  
+</div>
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=chinmaybhattt&label=Profile%20views&color=0e75b6&style=flat" alt="chinmaybhattt" /> </p>
+<!-- <h2 align="center">Leetcode Info</h2>   -->
+<p align="center">
+  <a href="https://leetcode.com/u/ChinmayBhattt/" target="_blank">
+
+
+   <img src="https://assets.leetcode.com/static_assets/marketing/202502.gif" height="100" width="100" ></img>
+<img src="https://assets.leetcode.com/static_assets/marketing/500_new.gif"  height="100" width="100"></img>
+<img src="https://assets.leetcode.com/static_assets/marketing/365.gif" height="100" width="100"></img>
+<img src="https://leetcode.com/static/images/badges/2024/gif/2024-11.gif" height="100" width="100"  ></img>
+<img src="https://leetcode.com/static/images/badges/2024/gif/2024-09.gif" height="100" width="100"  ></img>
+<img src="https://assets.leetcode.com/static_assets/marketing/2024.gif" height="100" width="100" ></img>
+<img src="https://assets.leetcode.com/static_assets/marketing/2024-100-new.gif" height="100" width="100"  ></img>
+<img src="https://assets.leetcode.com/static_assets/marketing/2023-50.gif" height="100" width="100"  ></img>
+
+</p>
+
+
+<!--# 📊 GitHub Stats -->
+<!--#<hr>-->
+<img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="1000">
+
+<div align="center">
+  <table>
+    <tr>
+      <td>
+        <!-- LeetCode Score -->
+        <img src="https://leetcard.jacoblin.cool/chinmaybhattt?theme=radical" alt="LeetCode Score" width="500" height="auto" />
+      </td>
+      <td>
+        <!-- GitHub Streak Stats -->
+<!--         <img src="https://github-readme-streak-stats.herokuapp.com/?user=ChinmayBhattt&theme=tokyonight" alt="Chinmay GitHub Streak" width="500" height="auto" /> -->
+         <a href="https://awesome-github-stats.azurewebsites.net/index.html??cardType=github&theme=github-dark&preferLogin=false&Text=C7C7C7&Border=DDDDDD&Ring=00F5FF&Background=171717&Title=DDDDDD">    <img  alt="ChinmayBhattt's GitHub Stats" src="https://awesome-github-stats.azurewebsites.net/user-stats/ChinmayBhattt?cardType=github&theme=github-dark&preferLogin=false&Text=C7C7C7&Border=DDDDDD&Ring=00F5FF&Background=171717&Title=DDDDDD" />  </a>
+      </td>
+    </tr>
+  </table>
+  
+</div>
+<table>
+  
+  <td>
+        <!-- Most Used Languages inside the border -->
+        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ChinmayBhattt&langs_count=8&theme=github_dark&layout=compact" alt="Top Languages" width="700" height="auto" />
+      </td>
+      <td>
+        <!-- GitHub Stats and Productive Time Card -->
+  <a href="https://awesome-github-stats.azurewebsites.net/index.html??cardType=level&theme=dark&preferLogin=false&Title=DDDDDD&Text=C2C2C2&Border=DDDDDD&Ring=66EAFF">    <img  alt="Chinmaybhattt's GitHub Stats" src="https://awesome-github-stats.azurewebsites.net/user-stats/Chinmaybhattt?cardType=level&theme=dark&preferLogin=false&Title=DDDDDD&Text=C2C2C2&Border=DDDDDD&Ring=66EAFF" />  </a>        <br>
+        <!-- GitHub Graph -->
+        <img src="https://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=ChinmayBhattt&theme=github_dark&utcOffset=8" alt="Productive Time" />
+      </td>
+    </tr>
+  </table>
+</div>
+
+<!-- Divider line -->
+<hr>
+
+### <img src="https://github.com/SP-XD/SP-XD/blob/main/images/message.gif?raw=true" width="30" /> About Me 
+
+- **First Year Student** in BTech (Artificial Intelligence & Data Science)  <img src="https://cdn.dribbble.com/userupload/19716588/file/original-e0d213e8642d6075915e55bb8adbd422.gif" min-width="350px" max-width="350px" width="450" align="right"> 
+- **3x Hackathon Participant** & Leader of @NeuroScript Team
+- **𝗖𝗼-𝗧𝗲𝗰𝗵 𝗟𝗲𝗮𝗱 & 𝗢𝗿𝗴𝗮𝗻𝗶𝘀𝗲𝗿** - @HackAryaVerse - @ACEIT
+- **LinkedIn**: **50k+** Impressions | **1k+** Engagements  
+- **Skilled in**:  
+  - Programming: `C++`, `Python`, `JavaScript`  
+  - Full Stack Development: `Node.js`, `Express.js`, `Next.js`, `React`, `MongoDB`, `Tailwind CSS`  
+- **Tools**:  
+  - `Figma`, `WordPress`, `Git/GitHub`, `Jupyter`, `MongoDB Atlas`  
+- **Exploring**: Rocket Science alongside Programming and Design  
+- **Growth**: Thriving through collaborative and individual projects  
+
+Feel free to connect or collaborate! 🚀  
+
+## 🔗 Connect With Me:  <img src="https://user-images.githubusercontent.com/74038190/216654128-ad1c5827-e18e-43a6-974b-3669cbb082b9.gif" width="40" />
+[![Instagram](https://img.shields.io/badge/-Instagram-E4405F?logo=instagram&logoColor=white&style=for-the-badge)](https://instagram.com/chinmaybhattt)
+[![Discord](https://img.shields.io/badge/-Discord-5865F2?logo=discord&logoColor=white&style=for-the-badge)](https://discord.gg/6RpzEHYdKt)
+[![Twitter](https://img.shields.io/badge/-Twitter-000000?logo=x&logoColor=white&style=for-the-badge)](https://x.com/chinmaybhattt)
+[![Linktree](https://img.shields.io/badge/-Linktree-43E660?logo=linktree&logoColor=white&style=for-the-badge)](https://linktr.ee/ChinmayBhattt)
+[![LeetCode](https://img.shields.io/badge/-LeetCode-FFA116?logo=leetcode&logoColor=white&style=for-the-badge)](https://leetcode.com/chinmaybhattt)
+[![LinkedIn](https://img.shields.io/badge/-LinkedIn-0077B5?logo=linkedin&logoColor=white&style=for-the-badge)](https://linkedin.com/in/chinmaybhattt)
+[![YouTube](https://img.shields.io/badge/-YouTube-FF0000?logo=youtube&logoColor=white&style=for-the-badge)](https://youtube.com/@chinmaybhattt)
+
+
+# 💻 Languages And Tools:
+
+   <a href="[![My Skills](https://skillicons.dev/icons?i=js,html,css,wasm)](https://skillicons.dev)">
+      <img src="https://skillicons.dev/icons?i=cpp,python,cs,javascript,mongodb,nodejs,react,sklearn,mysql,git,github,tailwindcss,c,wordpress,arduino" />
+  </a>
+<!--- <hr> --->
+<img src="https://user-images.githubusercontent.com/74038190/212284100-561aa473-3905-4a80-b561-0d28506553ee.gif" width="1000">
+
+
+----------------------------------------------------------------
+------------------------------------------------
+
+
+
+
+
+<table align="center">
+  <tr>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=js" width="55" height="55" /><br>JavaScript</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=react" width="55" height="55" /><br>React</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=nodejs" width="55" height="55" /><br>Node.js</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=express" width="55" height="55" /><br>Express.js</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=mongodb" width="55" height="55" /><br>MongoDB</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=html" width="55" height="55" /><br>HTML</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=typescript" width="55" height="55" /><br>Typescript</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=wordpress" width="55" height="55" /><br>Wordpress</td>
+  </tr>
+  <tr>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=css" width="55" height="55" /><br>CSS</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=tailwind" width="55" height="55" /><br>Tailwind CSS</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=bootstrap" width="55" height="55" /><br>Bootstrap</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=python" width="55" height="55" /><br>Python</td>
+    <td align="center" width="90"><img src="https://img.shields.io/badge/scikit-learn-orange" width="55" height="55" /><br>Scikit-Learn</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=cpp" width="55" height="55" /><br>C++</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=opencv" width="55" height="55" /><br>OpenCV</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=fastapi" width="55" height="55" /><br>Fastapi</td>
+  </tr>
+  <tr>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=git" width="55" height="55" /><br>Git</td>
+    <td align="center" width="90"><img src="https://img.shields.io/badge/pycharm-darkgreen" width="55" height="55" /><br>PyCharm</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=github" width="55" height="55" /><br>GitHub</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=vscode" width="55" height="55" /><br>VS Code</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=figma" width="55" height="55" /><br>Figma</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=discord" width="55" height="55" /><br>Discord</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=openrocket" width="55" height="55" /><br>OpenRocket</td>
+    <td align="center" width="90"><img src="https://skillicons.dev/icons?i=openrocket" width="55" height="55" /><br>OpenRocket</td>
+  </tr>
+  <tr>
+  </tr>
+   <tr>
+   
+</tr>
+
+</table>
